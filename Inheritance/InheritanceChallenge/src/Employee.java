b58a7749694eef30d278ae01a1c2a419958dd4af
imjustha <imjustha@gmail.com>
2024-03-29 14:05:23 -0500

    Employee number automatic

--- Inheritance/InheritanceChallenge/src/Employee.java
+++ Inheritance/InheritanceChallenge/src/Employee.java
@@ -2,9 +2,11 @@ public class Employee extends Worker {
     private long employeeId;
     private String hireDate;
 
-    public Employee(String name, String birthdate, long employeeId, String hireDate){
+    private static int employeeNo = 1;
+
+    public Employee(String name, String birthdate, String hireDate){
         super(name, birthdate);
-        this.employeeId = employeeId;
+        this.employeeId = Employee.employeeNo++;
         this.hireDate = hireDate;
     }
 
--- Inheritance/InheritanceChallenge/src/Main.java
+++ Inheritance/InheritanceChallenge/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        Employee hajeong = new Employee("Ha Jeong", "01/31/1998", "234234", "03/29/2024");
+        Employee hajeong = new Employee("Ha Jeong", "01/31/1998","03/29/2024");
         System.out.println(hajeong);
         System.out.println("Age = " + hajeong.getAge());
         System.out.println("Pay = " + hajeong.collectPay());
