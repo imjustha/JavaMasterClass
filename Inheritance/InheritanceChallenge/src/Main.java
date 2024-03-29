221a311a3cb28e1eae725b3b18fdb2ba3b4921c2
imjustha <imjustha@gmail.com>
2024-03-29 14:06:24 -0500

    add another employee to check

--- Inheritance/InheritanceChallenge/src/Main.java
+++ Inheritance/InheritanceChallenge/src/Main.java
@@ -4,6 +4,10 @@ public class Main {
         System.out.println(hajeong);
         System.out.println("Age = " + hajeong.getAge());
         System.out.println("Pay = " + hajeong.collectPay());
+
+        Employee joe = new Employee("Joe", "11/11/1990", "03/20/2024");
+        System.out.println(joe);
+
     }
 }
 
