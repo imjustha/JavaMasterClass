b28843c9d470e1bffd159212236418b20cf347e9
imjustha <imjustha@gmail.com>
2024-03-30 21:21:38 -0500

    The text block and other formatting options

---
+++ Inheritance/StringFormatting/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+
+        String bulletIt = "Print a Bulleted List:" +
+                "\u2022 First Point" +
+                "\u2022 Sub Point";
+
+        System.out.println(bulletIt);
+    }
+}
