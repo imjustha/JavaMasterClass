bfc4a5a3e6a36abcff68e2c4dcb0d07dd012fbb9
imjustha <imjustha@gmail.com>
2024-03-30 21:27:46 -0500

    Update

--- Inheritance/StringFormatting/src/Main.java
+++ Inheritance/StringFormatting/src/Main.java
@@ -20,7 +20,10 @@ public class Main {
         System.out.println(textBlock);
 
         int age = 35;
-        System.out.printf("Your age is %d", age);
+        System.out.printf("Your age is %d\n", age);
+
+        int yearOfBirth = 2023 - age;
+        System.out.printf("Age = %d, Birth year = %d", age, yearOfBirth);
 
     }
 }
