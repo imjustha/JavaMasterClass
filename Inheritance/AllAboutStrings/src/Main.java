46ed5c77be164033e141fe80600252e4510d88c4
imjustha <imjustha@gmail.com>
2024-03-31 00:53:45 -0500

    String empty

--- Inheritance/AllAboutStrings/src/Main.java
+++ Inheritance/AllAboutStrings/src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args){
         printInformation("Hello World");
+        printInformation("");
     }
 
     public static void printInformation(String string){
         int length = string.length();
         System.out.printf("Length = %d %n", length);
+        if(string.isEmpty()){
+            System.out.println("This string is empty");
+            return;
+        }
         System.out.printf("First char = %c %n", string.charAt(0));
-        
+
+        System.out.printf("Last char = %c %n", string.charAt(length - 1));
     }
 }
