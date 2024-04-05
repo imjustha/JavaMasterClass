ff278c26974a1c3489b1efe7625ec29243a51560
imjustha <imjustha@gmail.com>
2024-04-05 14:23:31 -0500

    Hamburger ordering system

---
+++ Polymorphism/BillsBurgerChallenge/src/MealOrder.java
@@ -0,0 +1,19 @@
+public class MealOrder {
+
+    private Burger burger;
+    private Item drink;
+    private Item side;
+
+    public MealOrder(){
+        this("regular", "coke", "fries");
+    }
+    public MealOrder(String burgerType, String drinkType, String sideType){
+        this.burger = new Burger(burgerType, 4.0);
+        this.drink = new Item("drink", drinkType, 1.00);
+        this.side = new Item("side", sideType, 1.50);
+    }
+
+    public double getTotalPrice(){
+        return side.getAdjustedPrice() + drink.getAdjustedPrice() + burger.getAdjustedPrice();
+    }
+}
