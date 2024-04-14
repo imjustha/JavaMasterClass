b924478a7a24a15c2af04a6b2948e57581ca4d82
imjustha <imjustha@gmail.com>
2024-04-14 17:50:34 -0500

    Update

--- Polymorphism/BillsBurgerChallenge/src/Main.java
+++ Polymorphism/BillsBurgerChallenge/src/Main.java
@@ -8,8 +8,19 @@ public class Main {
 //        Item avocado = new Item("Topping", "avocado", 1.50);
 //        avocado.printItem();
 
-        Burger burger = new Burger("regular", 4.00);
-        burger.addToppings("BACON", "CHEESE", "MAYO");
-        burger.printItem();
+//        Burger burger = new Burger("regular", 4.00);
+//        burger.addToppings("BACON", "CHEESE", "MAYO");
+//        burger.printItem();
+
+        MealOrder reguralMeal = new MealOrder();
+        reguralMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+        reguralMeal.setDrinkSize("LARGE");
+        reguralMeal.printItemizedList();
+
+        MealOrder secondMeal = new MealOrder("turkey", "7-up", "chili");
+        secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
+        secondMeal.setDrinkSize("SMALL");
+        secondMeal.printItemizedList();
+
     }
 }
--- Polymorphism/BillsBurgerChallenge/src/MealOrder.java
+++ Polymorphism/BillsBurgerChallenge/src/MealOrder.java
@@ -16,4 +16,19 @@ public class MealOrder {
     public double getTotalPrice(){
         return side.getAdjustedPrice() + drink.getAdjustedPrice() + burger.getAdjustedPrice();
     }
+
+    public void printItemizedList(){
+        burger.printItem();
+        drink.printItem();
+        side.printItem();
+        System.out.println("-".repeat(30));
+        Item.printItem("TOTAL PRICE", getTotalPrice());
+    }
+    public void addBurgerToppings(String extra1, String extra2, String extra3){
+        burger.addToppings(extra1, extra2, extra3);
+    }
+
+    public void setDrinkSize(String size){
+        drink.setSize(size);
+    }
 }
