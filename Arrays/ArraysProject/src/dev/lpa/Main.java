2914b788791b9cbd04a1794f5c90878b8195ab0b
imjustha <imjustha@gmail.com>
2024-04-15 20:04:39 -0500

    Array part2

--- Arrays/ArraysProject/src/dev/lpa/Main.java
+++ Arrays/ArraysProject/src/dev/lpa/Main.java
@@ -1,5 +1,7 @@
 package dev.lpa;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
         int[] myIntArray = new int[10];
@@ -22,10 +24,30 @@ public class Main {
         // another way
         int[] newArray;
         // newArray = {5, 4, 3, 2, 1}; this is not working since it has to put new int[] so
-        newArray = new int[] {5, 4, 3, 2, 1};
+//        newArray = new int[] {5, 4, 3, 2, 1};
+        newArray = new int[5];
         for (int i = 0; i < newArray.length; i++){
-            System.out.println(newArray[i] + " ");
+            newArray[i] = newArray.length - i;
+        }
+        for (int i = 0; i < newArray.length; i++){
+            System.out.print(newArray[i] + " ");
+        }
+        System.out.println();
+        for (int element : newArray){
+            System.out.print(element + " ");
+        }
+        System.out.println();
+        System.out.println(newArray);
+        System.out.println(Arrays.toString(newArray));
+
+        Object objectVariable = newArray;
+        if (objectVariable instanceof int[]){
+            System.out.println("objectVariable is really an int array");
         }
 
+        Object[] objectArray = new Object[3];
+        objectArray[0] = "Hello";
+        objectArray[1] = new StringBuilder("World");
+        objectArray[2] = newArray;
     }
 }
