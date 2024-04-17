94040a55386333cb0752efe9ba46552a99c86fad
imjustha <imjustha@gmail.com>
2024-04-17 01:54:10 -0500

    Multi dimensional arrays

--- Arrays/MultipleArrays/src/dev/lpa/Main.java
+++ Arrays/MultipleArrays/src/dev/lpa/Main.java
@@ -8,14 +8,14 @@ public class Main {
         System.out.println(Arrays.toString(array2));
         System.out.println("array2.length = " + array2.length);
 
-        for(int[] outer : array2){
+        for (int[] outer : array2) {
             System.out.println(Arrays.toString(outer));
         }
 
 //        Traditional multiple for loop
-        for(int i = 0; i < array2.length; i++){
+        for (int i = 0; i < array2.length; i++) {
             var innerArray = array2[i];
-            for(int j = 0; j < innerArray.length; j++){
+            for (int j = 0; j < innerArray.length; j++) {
 //                System.out.println(array2[i][j] + " ");
                 array2[i][j] = (i * 10) + (j + 1);
             }
@@ -32,5 +32,33 @@ public class Main {
 
         //another way to print
         System.out.println(Arrays.deepToString(array2));
+
+        // another way
+        array2[1] = new int[]{10, 20, 30};
+        System.out.println(Arrays.deepToString(array2));
+
+
+        Object[] anyArray = new Object[3];
+        System.out.println(Arrays.toString(anyArray));
+
+        anyArray[0] = new String[] {"a", "b", "c"};
+        System.out.println(Arrays.deepToString(anyArray));
+
+        anyArray[1] = new String[][]{
+                {"1", "2"},
+                {"3", "4", "5"},
+                {"6", "7", "8", "9"}
+        };
+        System.out.println(Arrays.deepToString(anyArray));
+
+        anyArray[2] = new int[2][2][2];
+        // anyArray[2] = "Hello"; this is not going to work
+        System.out.println(Arrays.deepToString(anyArray));
+
+        for(Object element : anyArray) {
+            System.out.println("Element type = " + element.getClass().getSimpleName());
+            System.out.println("Element toString() = " + element);
+            System.out.println(Arrays.deepToString((Object[]) element));
+        }
     }
 }
