d9fd7135d758a1c81ec49157e4ddeabc40c73513
imjustha <imjustha@gmail.com>
2024-04-18 00:01:42 -0500

    ArrayList part 2

--- Arrays/ArrayLists/src/dev/lpa/Main.java
+++ Arrays/ArrayLists/src/dev/lpa/Main.java
@@ -7,6 +7,11 @@ record GroceryItem(String name, String type, int count){
     public GroceryItem(String name){
         this(name, "Dairy", 1);
     }
+
+    @Override
+    public String toString(){
+        return String.format("%d %s in %s", count, name.toUpperCase(), type);
+    }
 }
 public class Main {
     public static void main(String[] args) {
@@ -24,5 +29,12 @@ public class Main {
         ArrayList<GroceryItem> groceryList = new ArrayList<GroceryItem>();
         groceryList.add(new GroceryItem("Butter"));
 //        groceryList.add("Yogurt");
+        groceryList.add(new GroceryItem("milk"));
+        groceryList.add(new GroceryItem("oranges", "Produce", 5));
+//        groceryList.add(0, new GroceryItem("apples", "produce", 6));
+
+        groceryList.set(0, new GroceryItem("apples", "produce", 6));
+        groceryList.remove(1);
+        System.out.println(groceryList);
     }
 }
